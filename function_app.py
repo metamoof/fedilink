f29fb758fc5d583488618653932947d930db23dc
Giles Antonio Radford <garadford@transcomashipping.com>
2023-01-06 23:49:05 +0100

    ✨ Creates three-cookie countdown, partly implementing #2

--- function_app.py
+++ function_app.py
@@ -1,13 +1,83 @@
+import http.cookies
 import logging
+import os
 
 import azure.functions as func
 
 app = func.FunctionApp()
 
 
+@app.function_name("home")
+@app.route("home")
+async def home(req: func.HttpRequest) -> func.HttpResponse:
+    cookiestring = req.headers.get("Cookie", "")
+    cookies = http.cookies.SimpleCookie(cookiestring)
+
+    resp = func.HttpResponse(body=f"Home Page.\n{cookies.output()}")
+    return resp
+
+
+@app.function_name("cookies_delete")
+@app.route("cookies/delete")
+async def cookies_delete(req: func.HttpRequest) -> func.HttpResponse:
+    cookiestring = req.headers.get("Cookie", "")
+    cookies = http.cookies.SimpleCookie(cookiestring)
+
+    resp = func.HttpResponse(status_code=302, headers={"Location": "/home"})
+
+    for key in cookies:
+        morsel = cookies[key]
+        del morsel["max-age"]
+        morsel["expires"] = "Thu, 01 Jan 1970 00:00:00 GMT"
+        morsel["path"] = "/"
+        cookies[key] = None
+        resp.headers.add("Set-Cookie", morsel.OutputString())
+    return resp
+
+
 @app.function_name(name="redirect")
 @app.route(route="{*destination}")
 async def redirect(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse(
-        status_code="302", headers={"Location": req.route_params["destination"]}
-    )
+    cookiestring = req.headers.get("Cookie", "")
+    cookies = http.cookies.SimpleCookie(cookiestring)
+    if "destination" not in req.route_params:
+        resp = func.HttpResponse(status_code=302, headers={"Location": "/home"})
+
+    elif cookies.get("instance_url"):
+        resp = func.HttpResponse(
+            status_code=302, headers={"Location": req.route_params["destination"]}
+        )
+    elif (
+        "input_requests_remaining" not in cookies
+        or int(cookies["input_requests_remaining"].value) < 0
+    ):
+        cookies["input_requests_remaining"] = (
+            int(os.environ.get("MaxInputRequests", "3")) - 1
+        )
+
+        resp = func.HttpResponse(
+            body=f"Request an instance. Remaining: {cookies['input_requests_remaining'].value}"
+        )
+    elif int(cookies["input_requests_remaining"].value) > 0:
+        cookies["input_requests_remaining"] = (
+            int(cookies.get("input_requests_remaining").value) - 1
+        )
+        resp = func.HttpResponse(
+            body=f"Request an instance. Remaining: {cookies['input_requests_remaining'].value}"
+        )
+
+    else:  # input_requests_remaining = 0
+        resp = func.HttpResponse(
+            status_code=302, headers={"Location": req.route_params["destination"]}
+        )
+
+    for morsel in cookies.values():
+        morsel["max-age"] = os.environ.get("CookieMaxAge", 5184000)  # default: 60 days
+        morsel["secure"] = True
+        morsel["httponly"] = True
+        morsel["samesite"] = "Strict"
+        morsel["version"] = 1
+        morsel["path"] = "/"
+        resp.headers.add("Set-Cookie", morsel.OutputString())
+
+    return resp
--- test_function_app.py
+++ test_function_app.py
@@ -1,7 +1,15 @@
+from typing import Any, Awaitable, Callable, Coroutine
+
 import pytest
 from azure import functions
 
 import function_app
+
+
+def unwrap_http_function(
+    func,
+) -> Callable[[functions.HttpRequest], Awaitable[functions.HttpResponse]]:
+    return func.build().get_user_function()
 
 
 @pytest.mark.asyncio
@@ -11,10 +19,75 @@ async def test_plain_redirect():
         url="http://localhost:7071/https://mas.to/@moof/109642947257051795",
         route_params={"destination": "https://mas.to/@moof/109642947257051795"},
         body=b"",
+        headers={
+            "Cookie": "input_requests_remaining=0; HttpOnly; SameSite=Strict; Secure; Version=1"
+        },
     )
     func = (
         function_app.redirect.build().get_user_function()
     )  # Gets through the wrappers
     resp = await func(req)
-    assert resp.status_code == "302"
+    assert resp.status_code == 302
+    assert resp.headers["Location"] == "https://mas.to/@moof/109642947257051795"
+
+
+@pytest.mark.asyncio
+async def test_instance_request_cookie():
+    req = functions.HttpRequest(
+        method="GET",
+        url="http://localhost:7071/https://mas.to/@moof/109642947257051795",
+        route_params={"destination": "https://mas.to/@moof/109642947257051795"},
+        body=b"",
+        headers={},
+    )
+    func = unwrap_http_function(function_app.redirect)  # Gets through the wrappers
+    resp = await func(req)
+    assert resp.status_code == 200
+    assert (
+        resp.headers["Set-Cookie"]
+        == "input_requests_remaining=2; HttpOnly; Max-Age=5184000; SameSite=Strict; Secure; Version=1"
+    )
+    req = functions.HttpRequest(
+        method="GET",
+        url="http://localhost:7071/https://mas.to/@moof/109642947257051795",
+        route_params={"destination": "https://mas.to/@moof/109642947257051795"},
+        body=b"",
+        headers={"Cookie": resp.headers["Set-Cookie"]},
+    )
+    resp = await func(req)
+    assert resp.status_code == 200
+    assert (
+        resp.headers["Set-Cookie"]
+        == "input_requests_remaining=1; HttpOnly; Max-Age=5184000; SameSite=Strict; Secure; Version=1"
+    )
+
+    req = functions.HttpRequest(
+        method="GET",
+        url="http://localhost:7071/https://mas.to/@moof/109642947257051795",
+        route_params={"destination": "https://mas.to/@moof/109642947257051795"},
+        body=b"",
+        headers={
+            "Cookie": "input_requests_remaining=1; HttpOnly; SameSite=Strict; Secure; Version=1; Max-Age=5183210"
+        },
+    )
+    resp = await func(req)
+    assert resp.status_code == 200
+    assert (
+        resp.headers["Set-Cookie"]
+        == "input_requests_remaining=0; HttpOnly; Max-Age=5184000; SameSite=Strict; Secure; Version=1"
+    )
+
+    req = functions.HttpRequest(
+        method="GET",
+        url="http://localhost:7071/https://mas.to/@moof/109642947257051795",
+        route_params={"destination": "https://mas.to/@moof/109642947257051795"},
+        body=b"",
+        headers={"Cookie": resp.headers["Set-Cookie"]},
+    )
+    resp = await func(req)
+    assert resp.status_code == 302
+    assert (
+        resp.headers["Set-Cookie"]
+        == "input_requests_remaining=0; HttpOnly; Max-Age=5184000; SameSite=Strict; Secure; Version=1"
+    )
     assert resp.headers["Location"] == "https://mas.to/@moof/109642947257051795"
