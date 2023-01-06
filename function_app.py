ac89f7b22e854e67c79047cfc1ebb0d279a8e735
Giles Antonio Radford <garadford@transcomashipping.com>
2023-01-06 20:27:41 +0100

    🎨 Changed everything to Async. Closes #3

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 @app.function_name(name="redirect")
 @app.route(route="{*destination}")
-def redirect(req: func.HttpRequest) -> func.HttpResponse:
+async def redirect(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
         status_code="302", headers={"Location": req.route_params["destination"]}
     )
--- test_function_app.py
+++ test_function_app.py
@@ -1,9 +1,11 @@
+import pytest
 from azure import functions
 
 import function_app
 
 
-def test_plain_redirect():
+@pytest.mark.asyncio
+async def test_plain_redirect():
     req = functions.HttpRequest(
         method="GET",
         url="http://localhost:7071/https://mas.to/@moof/109642947257051795",
@@ -13,17 +15,6 @@ def test_plain_redirect():
     func = (
         function_app.redirect.build().get_user_function()
     )  # Gets through the wrappers
-    resp = func(req)
+    resp = await func(req)
     assert resp.status_code == "302"
     assert resp.headers["Location"] == "https://mas.to/@moof/109642947257051795"
-
-
-def test_route():
-    req = functions.HttpRequest(
-        method="GET",
-        url="http://localhost:7071/https://mas.to/@moof/109642947257051795",
-        route_params={"destination": "https://mas.to/@moof/109642947257051795"},
-        body=b"",
-    )
-    app = functions.AsgiFunctionApp(function_app.app)
-    app.http_
